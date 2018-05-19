f1bab8e4ff2c1c32581bc4c36974e4b29a47ea8a
vsalles79 <39140076+vsalles79@users.noreply.github.com>
2018-05-19 16:05:55 -0300

    vinicius
    listar

--- listar.php
+++ listar.php
@@ -74,7 +74,37 @@
 				</thead>
 		        <tbody>
 
-		        </tbody>               
+		        </tbody>
+		        <?php
+					if(file_exists("init.php")) {
+						require "init.php";		
+					} else {
+						echo "Arquivo init.php nao foi encontrado";
+						exit;
+					}
+
+					if(!function_exists("Abre_Conexao")) {
+						echo "Erro o arquivo init.php foi auterado, nao existe a função Abre_Conexao";
+						exit;
+					}
+
+					Abre_Conexao();
+					$re = mysql_query("SELECT * FROM usuarios ORDER BY usuarios.nome;");
+					if(mysql_errno() != 0) {
+						if(!isset($erros)) {
+							echo "Erro o arquivo init.php foi auterado, nao existe $erros";
+							exit;
+						}
+						echo $erros[mysql_errno()];
+						exit;
+					}
+					?>
+
+					<table width="1600" border="1">
+						<tr>
+							<td>A&ccedil;&otilde;es</td>
+							<td>Nome</td>		
+						</tr>              
 				<?php
 				while($l = mysql_fetch_array($re)) {
 					$id           = $l["id_usuario"];
