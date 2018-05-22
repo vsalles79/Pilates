0fa711933e1018f91948e0309ca9b4f91b961342
vsalles79 <39140076+vsalles79@users.noreply.github.com>
2018-05-21 22:13:05 -0300

    acesso banco

--- PHPMailer/definedb.php
+++ PHPMailer/definedb.php
@@ -2,5 +2,5 @@
 define("SERVIDOR", "mysql.hostinger.com.br");
 define("USUARIO", "u828469361_user");
 define("SENHA", "ACESSObanco");
-define("BANCO", "u828469361_bd");
+define("BANCO", "u828469361_db");
 ?>
