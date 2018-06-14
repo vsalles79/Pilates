5da06ff8b0258f196c08d11177e33fe473778fc6
Rafael Motomura <mo_tomura@hotmail.com>
2018-06-13 22:22:03 -0300

    retirado cdn para teste

--- cadastroAluno.php
+++ cadastroAluno.php
@@ -167,7 +167,6 @@ function SomenteNumero(e){
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.13/jquery.mask.min.js"></script>
 
 
